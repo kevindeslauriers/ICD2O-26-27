1ff5ac3aafcac6ea4f467554b00db5e9d9fb2dfe
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2026-09-22 13:46:24 -0400

    End of Lesosn on Variables and Data Types

--- Unit 1/Day 3/example1.py
+++ Unit 1/Day 3/example1.py
@@ -1,1 +1,40 @@
-lives = 3
+score = 0       # data_type is int 
+lives = 3       # data_type is int
+player_name = "Nova"    #player_name is assigned the value "Nova"   data_type is String (str)
+
+light_on = True      #data_type is Boolean (bool)
+has_shoes = "False"  #data_types is str
+
+distance = 6.5      #data_type is float (floating point number)
+average_mark = 87.3     #data_type is float (floating point number)
+
+print(score, type(score))
+print(lives, type(lives))
+print(player_name, type(player_name))
+print(light_on, type(light_on))
+print(has_shoes, type(has_shoes))
+print(distance, type(distance))
+print(average_mark, type(average_mark))
+
+
+print(7 + 2.0)  # int + float => float
+print(8/4)          # division / always gives a float
+print(8//4)
+print(1/2)
+print(1//2)
+
+print(7/2)      # regular division -> float
+print(7//2)     # integer division -> int (truncates the answer -- chop off the decimal)
+
+print("Alpha" + "bet")
+print("Alpha","bet")
+
+new_word = "Alpha" + "bet"      # new_world is assigned "Alphabet"
+print(new_word)
+
+# print("Lives" + True)
+
+
+
+
+
